b2dd90ec4136cb3ecf4522d96693af17c222b006
JaretWright <hkkA223>
2018-04-11 13:14:03 -0400

    Added Clipping to Image

--- src/w18comp1008s3apr11/ItsAboutMeViewController.java
+++ src/w18comp1008s3apr11/ItsAboutMeViewController.java
@@ -2,7 +2,11 @@ package w18comp1008s3apr11;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller class
@@ -11,14 +15,21 @@ import javafx.fxml.Initializable;
  */
 public class ItsAboutMeViewController implements Initializable
 {
-
+    @FXML private ImageView imageView;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        imageView.setImage(new Image("JW.jpg"));
+        
+        //set up a "clip" to apply a rounded border to the image
+        Rectangle clip = new Rectangle(imageView.getFitWidth(), 
+                                       imageView.getFitHeight());
+        clip.setArcWidth(60);
+        clip.setArcHeight(60);
+        imageView.setClip(clip);
     }    
-    
 }
