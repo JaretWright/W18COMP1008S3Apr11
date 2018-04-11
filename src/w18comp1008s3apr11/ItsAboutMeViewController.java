b5ebe5d1801943b687e682923fd4bcecd25e2de9
JaretWright <hkkA223>
2018-04-11 12:24:13 -0400

    Initial Upload

---
+++ src/w18comp1008s3apr11/ItsAboutMeViewController.java
@@ -0,0 +1,24 @@
+package w18comp1008s3apr11;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author JWright
+ */
+public class ItsAboutMeViewController implements Initializable
+{
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+    
+}
---
+++ src/w18comp1008s3apr11/LaunchGUI.java
@@ -0,0 +1,30 @@
+package w18comp1008s3apr11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author JWright
+ */
+public class LaunchGUI extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("ItsAboutMeView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("It's me");
+        primaryStage.show();
+    }
+    
+}
